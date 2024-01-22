a4f68b641bf744b4ccbca5b8533ea815ce25d54e
vigneshpraveen11 <rpraveen@usc.edu>
2024-01-21 16:54:15 -0800

    Added one more cypress test

--- cypress/e2e/App.e2e.cy.js
+++ cypress/e2e/App.e2e.cy.js
@@ -4,4 +4,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('a pokemon\'s page can be viewed', function() {
+    cy.visit('http://localhost:3000')
+    cy.get('a[href*="/pokemon/ivysaur"]').click()
+    cy.contains('ivysaur')
+    cy.contains('chlorophyll')
+  })
 })
